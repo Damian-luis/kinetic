388f2df660afbbc58f30cecd9f56977d1ccb115e
Damian-luis <damian.luis.porta@gmail.com>
2022-07-29 15:19:20 -0300

    changess

--- src/componentes/About.jsx
+++ src/componentes/About.jsx
@@ -1,5 +1,30 @@
 import React from "react";
-
+import "./About.css"
 export default function About(){
-    return (<div className="about"></div>)
+    return (<div className="about">
+        <div className="somos">
+            <h1>SOMOS</h1><br/>
+            <p>Somos una comunidad creativa llevando endorfinas a cada rincón del mundo. Creemos que las historias de éxito son un medio para alcanzar la felicidad y somos el medio que impulsa a marcas excepcionales a superar sus metas. Nuestro equipo está conformado por 50 creativos multidisciplinarios entre los 18 y 38 años, con un orgulloso 60% de mujeres.</p>
+        </div>
+        <div className="video"><p>Espacio para logos/imagen/video</p></div>
+        <div className="first-section">
+        <img src={require("../assets/about1.jpg")} className="card"/>
+        <p>Nuestra Historia<br/>
+ēndor nació de la búsqueda de la innovación, la experimentación en diferentes áreas creativas y la misión de provocar felicidad.
+
+Hemos creado más de 900 marcas de todas partes del mundo.
+Hemos generado más de 700 minutos de video, más de 25 millones de views en redes sociales e incontables endorfinas en clientes y colaboradores.</p>
+        </div>
+        <div className="second-section">
+        <img src={require("../assets/about2.jpg")} className="card"/>
+        <p>Cooltura<br/>
+Todo lo que conformamos, desde nuestro equipo e instalaciones, hasta nuestras entregas y resultados, está creado para despertar endorfinas.
+
+Nuestra cooltura es la libertad creativa de experimentar, la confianza de expresarnos como somos y la apertura para hacer de nuestros clientes, amigos.</p>
+        </div>
+        <div className="our-filosofy">
+        Nuestra Filosofía<br/>
+Become Better® se basa en pasar del decir al hacer y del plan a la acción. Sabemos que todo en este mundo comienza por atreverse a hacer. Lo que de verdad deja huella es soltar los temores, emprender proyectos y apoyarnos para salir adelante. Nuestro más grande orgullo es convertir el “algún día lo haré” en un “me encanta hacerlo”.
+        </div>
+    </div>)
 }
--- src/componentes/Contact.jsx
+++ src/componentes/Contact.jsx
@@ -1,5 +1,135 @@
-import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import "./Contact.css"
+import axios, { Axios } from 'axios';
+import { useState } from 'react';
+import swal from "sweetalert";
+function Contact() {
 
-export default function Contact(){
-    return (<div className="contact"></div>)
+
+    
+   
+    
+  
+
+
+  //funcion succes con alert
+
+  function successButton(){
+
+    swal("THANK YOU!", "Your message was sent successfully", "success")
+    document.getElementById("form-reset").reset();
+  }
+
+    const[state,setState]=useState({
+        name:"",
+        phone:"",
+        mail:"",
+        service:"",
+        payment:"",
+        web:""
+    })
+    function nameHandler(e){
+        setState({...state,name:e.target.value})
+    }
+    function phoneHandler(e){
+        setState({...state,phone:e.target.value})
+    }
+    function mailHandler(e){
+        setState({...state,mail:e.target.value})
+    }
+    function serviceHandler(e){
+        setState({...state,service:e.target.value})
+    }
+    function paymentHandler(e){
+        setState({...state,payment:e.target.value})
+    }
+    function webHandler(e){
+        setState({...state,web:e.target.value})
+    }
+
+
+
+
+    async function sendHandler(e){
+        e.preventDefault()
+        if (state.name.length>1) {
+          if(state.mail.length>1){
+            if(state.phone.length>1){
+              if(state.service.length>1){
+                if(state.payment.length>=1){
+                  
+                  try{successButton()
+                    setState({
+                      name:"",
+                      phone:"",
+                      mail:"",
+                      service:"",
+                      payment:"",
+                      web:""
+                  })
+                  await axios.post("https://kineticc.herokuapp.com/enviar",state)
+                      
+                       }
+                      catch(error){}
+                    }
+                else{swal("SOMETHING WENT WRONG", "Please complete all required fields before submit", "warning")}
+              }
+              else{swal("SOMETHING WENT WRONG", "Please complete all required fields before submit", "warning")}
+            }
+            else{swal("SOMETHING WENT WRONG", "Please complete all required fields before submit", "warning")}
+        }
+        else{swal("SOMETHING WENT WRONG", "Please complete all required fields before submit", "warning")}
+        }
+        else{
+            swal("SOMETHING WENT WRONG", "Please complete all required fields before submit", "warning")
+          //try{console.log("enviando informacion...")
+          //await axios.post("https://whispering-depths-85783.herokuapp.com/enviar",{name,company,mail,number,message}) }
+           //catch(error){}
+        //}
+        }
+        
+        
+        
+    }
+
+  return (
+    <Form onSubmit={sendHandler} id="form-reset">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Write your name" onChange={nameHandler} value={state.name}/>
+        <Form.Label>Phone number</Form.Label>
+        <Form.Control type="number" placeholder="Enter your number" onChange={phoneHandler} value={state.phone}/>
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" onChange={mailHandler} value={state.mail}/>
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.<br />
+        </Form.Text>
+        <Form.Label>Service</Form.Label>
+        <Form.Select aria-label="Default select example" onChange={serviceHandler} value={state.service}>
+      <option>Open this select menu</option>
+      <option value="branding">Branding</option>
+      <option value="marketing">Marketing</option>
+      <option value="web">Web</option>
+    </Form.Select>
+    <Form.Label>Quote</Form.Label>
+    <Form.Select aria-label="Default select example" onChange={paymentHandler} value={state.payment}>
+      <option>Open this select menu</option>
+      <option value="valor1">valor1</option>
+      <option value="valor2">valor2</option>
+      <option value="valor3">valor3</option>
+    </Form.Select>
+    <Form.Label>website (optional)</Form.Label>
+        <Form.Control type="text" placeholder="Write a web as a reference" onChange={webHandler} />
+      </Form.Group>
+
+      
+      
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
+
+export default Contact;
